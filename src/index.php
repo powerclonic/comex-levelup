a4765803e5ffeba74b6cbe6483b37709fc59e189
Matheus Dresch <matheus.dresch02@gmail.com>
2023-10-08 18:54:58 -0300

    Gerenciando estoque

--- src/index.php
+++ src/index.php
@@ -1,24 +1,41 @@
 <?php
 
 $produtos = [
-    [
+    [ // 0
         'nome' => 'Caneta',
-        'preco' => 1.99
+        'preco' => 1.99,
+        'qtd_estoque' => 250
     ],
-    [
+    [ // 1
         'nome' => 'Caderno',
-        'preco' => 24.99
+        'preco' => 24.99,
+        'qtd_estoque' => 100
     ],
-    [
+    [ // 2
         'nome' => 'Processador',
-        'preco' => 249.99
+        'preco' => 249.99,
+        'qtd_estoque' => 70
     ],
-    [
+    [ // 3
         'nome' => 'Placa de vídeo',
-        'preco' => 1399.99
+        'preco' => 1399.99,
+        'qtd_estoque' => 15
     ]
 ];
 
 foreach ($produtos as $produto) {
-    echo "O produto " . $produto['nome'] . " custa R$" . $produto['preco'] . PHP_EOL;
+    echo "O produto " . $produto['nome'] . " custa R$" . $produto['preco'] . " e possui " . $produto['qtd_estoque'] . " item(s) em estoque." . PHP_EOL;
 };
+
+function compraProduto(int $idProduto, array &$produtos)
+{
+    $produtos[$idProduto]['qtd_estoque'] -= 1;
+}
+
+function repoeProduto(int $idProduto,  int $quantidade, array &$produtos)
+{
+    $produtos[$idProduto]['qtd_estoque'] += $quantidade;
+}
+
+compraProduto(0, $produtos);
+repoeProduto(0, 10, $produtos);
