6c634bc48b57a9263d6287c7f09bf95577fc8dad
Matheus Dresch <matheus.dresch02@gmail.com>
2023-10-08 21:04:26 -0300

    Criando a classe Pedido

--- src/index.php
+++ src/index.php
@@ -190,3 +190,65 @@ $cliente = new Cliente(
         'numero' => 987
     ]
 );
+
+class Pedido
+{
+    public function __construct(
+        private int $id,
+        private Cliente $cliente,
+        private array $produtos,
+    ) {
+    }
+
+    //? getters
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getCliente()
+    {
+        return $this->cliente;
+    }
+
+    public function getProdutos()
+    {
+        return $this->produtos;
+    }
+
+    public function getValorTotal()
+    {
+        return $this->calculaDesconto(array_reduce($this->produtos, fn ($carry, $current) => $carry += $current->getPreco()));
+    }
+
+    //? setters
+
+    public function setId(int $valor)
+    {
+        $this->id = $valor;
+    }
+
+    public function setCliente(Cliente $valor)
+    {
+        $this->cliente = $valor;
+    }
+
+    public function setProdutos(array $valor)
+    {
+        $this->produtos = $valor;
+    }
+
+    //? outros métodos
+
+    private function calculaDesconto(float $valorTotal)
+    {
+        if ($valorTotal < 100) return $valorTotal;
+
+        return number_format($valorTotal * 0.9, 2, ',', '.');
+    }
+}
+
+$pedido = new Pedido(1, $cliente, $produtos);
+
+echo "O pedido de ID " . $pedido->getId() . " custa no total R$" . $pedido->getValorTotal() . PHP_EOL;
